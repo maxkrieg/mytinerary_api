951f21a7e2a3abde9305bdd2a0632a7bc0f27c9c
maxkrieg <maxwell.krieg@gmail.com>
2015-06-06 20:58:01 -0400

    updated serializer for bad images

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -2,8 +2,8 @@ class EventSerializer < ActiveModel::Serializer
   attributes :id, :title, :date, :location, :start_time, :end_time, :attendees, :desc, :image_tag
 
   def image_tag
-    "<img class='media-object' src='https://s3.amazonaws.com/mytinerary_images/medium/#{object.id}/#{object.image_file_name}'/>"
-
+    if object.image_file_name
+      "<img class='media-object' src='https://s3.amazonaws.com/mytinerary_images/medium/#{object.id}/#{object.image_file_name}'/>"
+    end
   end
-
 end
